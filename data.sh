ed1bc78843e389c3f503cffe746a810ebc0c5dfa
itachi uchiha <114676214+itachiuchiha06@users.noreply.github.com>
2023-03-06 12:40:18 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 sudo yum update
 sudo yum -y install git
-git clone https://github.com/itachiuchiha06/docker-compose-for-wordpress.git
 sudo amazon-linux-extras install docker -y
 sudo yum install docker -y
 sudo service docker start
 sudo usermod -a -G docker ec2-user
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-chmod 666 /var/run/docker.sock
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-docker-compose --version
+sudo git clone https://github.com/itachiuchiha06/docker-compose-for-wordpress.git
+cd docker-compose-for-wordpress/
+docker-compose up -d                 
