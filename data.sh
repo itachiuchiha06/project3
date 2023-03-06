b2f6b6113ddab62e20390df470e15d7864ba69f3
itachi uchiha <114676214+itachiuchiha06@users.noreply.github.com>
2023-03-06 12:32:02 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -8,5 +8,6 @@ sudo service docker start
 sudo usermod -a -G docker ec2-user
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+chmod 666 /var/run/docker.sock
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 docker-compose --version
