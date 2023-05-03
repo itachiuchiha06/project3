b4d9a323713384301618337d0bb1eaad62a833b5
itachi uchiha <114676214+itachiuchiha06@users.noreply.github.com>
2023-05-03 10:36:16 +0530

    Delete provider.tf

--- provider.tf
+++
@@ -1,9 +0,0 @@
-# update provider
-# AWS as my provider
-
-provider "aws" {
-  region     = "us-east-1"
-  access_key = "AKIAXVIZWZTOAT3X64G5"
-  secret_key = "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"
-}
-
